939d1d408bb481999e08c24b0a309fa761c8cdbb
dylan98ed <dylanquiroga98@gmail.com>
2025-03-22 14:46:44 -0300

    add custom model

--- CV/main.py
+++ CV/main.py
@@ -1,12 +1,23 @@
 import cv2
 from object_detection import ObjectDetectionProcessor  # Asegúrate de que el archivo esté correctamente nombrado como `object_detection.py`
+from ultralytics import YOLO
+import os
 
 def main():
+
+    yolo_weights = "train_model/train/weights/best.pt"  # Nueva ruta del modelo entrenado
+    yolo_path = "train_model/train/weights/best_ncnn_model"
+
+    if not os.path.exists(yolo_path):
+        model = YOLO(yolo_weights)  # Cargar el modelo desde la nueva ubicación
+        model.export(format="ncnn")  # Exportar modelo a formato NCNN
+
+
     # Crear una instancia del procesador de detección de objetos
-    processor = ObjectDetectionProcessor()
+    processor = ObjectDetectionProcessor(yolo_path)
 
     # Cargar el video
-    cap = cv2.VideoCapture(2)#"data/Señal de Stop.mp4")  # Puedes cambiar por `0` para usar la cámara de la notebook
+    cap = cv2.VideoCapture("data/pista con niebla.mp4")#0)#"data/Señal de Stop.mp4")  # Puedes cambiar por `0` para usar la cámara de la notebook
 
     while cap.isOpened():
         status, frame = cap.read()  # Leer un frame del video
--- CV/object_detection.py
+++ CV/object_detection.py
@@ -1,59 +1,64 @@
 import cv2
 import torch
+from ultralytics import YOLO
 
 class ObjectDetectionProcessor:
 
-    def __init__(self):
-        # Cargar el modelo YOLOv5 nano
-        self.model = torch.hub.load("ultralytics/yolov5", "yolov5n", pretrained=True)
+    def __init__(self, yolo_path):
+        # Cargar el modelo YOLOv11 personalizado
+        self.model_path = yolo_path
+        self.model = YOLO(self.model_path)  
+        #self.model = YOLO('train_model/train/weights/best.pt')        # Ruta al modelo entrenado
+        self.class_names = self.model.names # Obtener los nombres de las clases
+
         # Dimensiones reales promedio de una señal de stop (en metros)
-        self.real_width = 0.06  # Ejemplo: 6 cm de ancho real
-        self.focal_length = 480  # Focal length en píxeles (deberías calibrar tu cámara)
+        self.real_width = 0.06  # señal de stop: 6 cm de ancho real
+        self.focal_length = 480  # Focal length en píxeles de webcam
 
     def calculate_distance(self, bbox_width):
-        """
-        Calcula la distancia a la señal de stop usando la relación entre el ancho real y el ancho en la imagen.
-
-        Args:
-            bbox_width (int): Ancho de la caja delimitadora en píxeles.
-
-        Returns:
-            float: Distancia a la señal de stop en metros.
-        """
+        """ Calcula la distancia a la señal de stop """
         distance_m = (self.real_width * self.focal_length) / bbox_width
-        distance_cm = distance_m * 100  # Convertir a centímetros
-        return distance_cm
+        return distance_m * 100  # Convertir a centímetros
 
     def process_image(self, cv_image):
         """
-        Procesa la imagen de entrada para detectar solo señales de stop y muestra la distancia a ellas.
-
-        Args:
-            cv_image (np.ndarray): Imagen de entrada.
-
-        Returns:
-            np.ndarray: Imagen procesada con cuadros delimitadores y distancias.
+        Procesa la imagen de entrada para detectar señales de stop y mostrar la distancia a ellas.
         """
-        # Inferencia con YOLOv5
-        pred = self.model(cv_image)
-        
-        # Convertir predicciones a un DataFrame y filtrar por confianza
-        df = pred.pandas().xyxy[0]
-        df = df[(df["confidence"] > 0.5) & (df["name"] == "stop sign")]
+        # Realizar la inferencia con YOLOv11
+        # Inference
+        results = self.model(cv_image)
 
-        # Dibujar las cajas de detección en el frame
-        for i in range(df.shape[0]):
-            bbox = df.iloc[i][["xmin", "ymin", "xmax", "ymax"]].values.astype(int)
-            bbox_width = bbox[2] - bbox[0]
-            distance_cm = self.calculate_distance(bbox_width)
-            
-            cv2.rectangle(cv_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-            cv2.putText(cv_image,
-                        f"Stop Sign: {round(df.iloc[i]['confidence'], 4)}, Dist: {round(distance_cm, 2)}cm",
-                        (bbox[0], bbox[1] - 15),
-                        cv2.FONT_HERSHEY_PLAIN,
-                        1,
-                        (255, 255, 255),
-                        2)
+        # Get detections and draw bounding boxes
+        for result in results:
+            boxes = result.boxes.xyxy.numpy()
+            confidences = result.boxes.conf.numpy()
+            labels = result.boxes.cls.numpy().astype(int)
+
+            valid_distance = True  # Default
+
+            for i, bbox in enumerate(boxes):
+                if confidences[i] < 0.5:
+                    continue
+
+                class_name = self.class_names[labels[i]]
+
+                bbox = bbox.astype(int)
+                bbox_width = bbox[2] - bbox[0]
+                distance_cm = self.calculate_distance(bbox_width)
+
+                bbox_color = (255, 0, 0) if distance_cm < 40 else (0, 0, 255)   #blue bbox if the distance is less than 40cm, red if it is greater
+                valid_distance = distance_cm >= 40
+
+                # Draw bbox
+                cv2.rectangle(cv_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), bbox_color, 2)
+
+                # Draw text with name, confidence, and distance
+                cv2.putText(cv_image,
+                            f"{class_name}: {round(confidences[i], 4)}, Dist: {round(distance_cm, 2)}cm",
+                            (bbox[0], bbox[1] - 15),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            1,
+                            (255, 255, 255),
+                            2)
 
         return cv_image
